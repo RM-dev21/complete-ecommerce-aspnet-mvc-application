3ace969bbff8837d22a1d9d3d5a7ccb5a9e370dd
Rumiya Makovetskaya <rumakovet@gmail.com>
2023-01-03 18:29:36 +0600

    Created Cinemas/Index.cshtml.

--- eTickets/Controllers/CinemasController.cs
+++ eTickets/Controllers/CinemasController.cs
@@ -15,8 +15,8 @@ namespace eTickets.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var data = await _context.Producers.ToListAsync();
-            return View();
+            var data = await _context.Cinemas.ToListAsync();
+            return View(data);
         }
     }
 }
