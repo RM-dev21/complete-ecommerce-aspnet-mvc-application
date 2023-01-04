8c88cf3f6e3b061f3976532647775fa0136fbd13
Rumiya Makovetskaya <rumakovet@gmail.com>
2023-01-04 20:09:36 +0600

    Created Data/Services/IActorsService to manipulate data from/for database without controller.

---
+++ eTickets/Data/Services/IActorsService.cs
@@ -0,0 +1,14 @@
+﻿using eTickets.Models;
+using System.Collections.Generic;
+
+namespace eTickets.Data.Services
+{
+    public interface IActorsService
+    {
+        IEnumerable<Actor> GetAll(); //all actors from db
+        Actor GetById(int id); // one actor from db
+        void Add(Actor actor); // add data to db
+        Actor Update(int id, Actor newActor); // update data in db
+        void Delete(int id); // delete data
+    }
+}
